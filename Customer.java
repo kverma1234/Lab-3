3e75f8352d444b5dd68756593129ace6f46faf4b
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-18 15:48:23 -0700

    Harsh contribution

--- Customer.java
+++ Customer.java
@@ -1,4 +1,7 @@
+
+//Harsh changes made to the code
 package restaurant;
+
 // Customer class
 public class Customer {
     private String name;
@@ -9,8 +12,24 @@ public class Customer {
         this.order = new Order();  // Collaborating with Order class
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
     public void addMenuItemToOrder(MenuItem item) {
         order.addItem(item);  // Collaborating with Order to add items
+    }
+
+    public void removeMenuItemFromOrder(MenuItem item) {
+        order.removeItem(item);  // Collaborating with Order to remove items
+    }
+
+    public void clearOrder() {
+        order.clear();  // Collaborating with Order to clear all items
     }
 
     public void placeOrder() {
