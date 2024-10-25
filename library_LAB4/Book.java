da5fb6e94fcf77f2bb39acb090356221b785d764
BaileyM4556 <baileymawhinney4556@gmail.com>
2024-10-25 16:55:42 -0700

    Updated Book.java file
    Added getters for each attribute and an updateStatus() method to update availability.

--- library_LAB4/Book.java
+++ library_LAB4/Book.java
@@ -5,8 +5,23 @@ public class Book {
 	// GRASP Principle: Information Expert
 	// Manages its own state (available or borrowed).
 	private String title;
+	private String author;
 	private boolean isAvailable; 
 	
 	// TODO: implement Book functionality 
+	public String getTitle() {
+		return title;
+	}
 	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public boolean getAvailability() {
+		return isAvailable;
+	}
+	
+	public void updateStatus() {
+		isAvailable = !isAvailable;
+	}
 }
